31467e5c2507d43d05811028afa8b494dc164051
isrdevgonzalez-hub <isr.g.lopez@gmail.com>
2026-08-02 14:03:30 -0400

    Initial commit

---
+++ dayflow-api/src/main/java/cl/dayflow/api/DayflowApiApplication.java
@@ -0,0 +1,13 @@
+package cl.dayflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayflowApiApplication.class, args);
+	}
+
+}
---
+++ dayflow-api/src/test/java/cl/dayflow/api/DayflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.dayflow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
